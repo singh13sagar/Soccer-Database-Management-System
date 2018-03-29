1748ec37979f2ba7d2ddc9048916ae6ef4f3e6e9
Sagar Singh <singh13sagar@gmail.com>
2018-03-29 01:52:42 -0400

    added some lines

--- trial.java
+++ trial.java
@@ -3,5 +3,9 @@ public class trial
     public static void main(String[] args)
     {
         System.out.println("Holaaaaa mi amigos");
+        System.out.println("Made some changes eh");
+
+
+        
     }
 }
