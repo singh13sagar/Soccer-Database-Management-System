d5864e0f110f57ebc1d789794dcd984609eb7d90
e2sanche <38050684+e2sanche@users.noreply.github.com>
2018-04-04 14:46:32 -0400

    Update Member.java
    updated

--- Member.java
+++ Member.java
@@ -1,126 +1,147 @@
-//compilethis
-
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.util.*;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.util.Random; // debug 
 import java.io.*;
 
-public class coachViewer extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					coachViewer frame = new coachViewer();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+public class Member implements Comparable<Member>{
+	private String Fname;
+	private String Lname;
+	private int pAttended; // # of practices attended
+	private String Address;
+	private int pBalance; // payment balance
+	private String phoneNumber;
+	Random rand = new Random();  // debug
+	private int timesPaid;
+	private boolean paid;
+	private String activity;
+	private String Message;
+	
+	public static void main(String[] args){
+		Member mem = new Member("Prabagar","Sivakumar");
+		mem.setNumber("647-555-5555");
+		mem.setPaymentStatus(true);
+		mem.setAddress("whatever");
+		mem.setMessage("No message");
+		mem.addMember();
+		
+		Member mem2 = new Member("Sagur","Singh");
+		mem2.setNumber("647-555-5555");
+		mem2.setPaymentStatus(true);
+		mem2.setAddress("whatever");
+		mem2.setMessage("No message");
+		mem2.addMember();
+	}
+	
+	public Member(String Fname, String Lname) 
+	{
+		this.Fname = Fname;
+		this.Lname = Lname;
+		pAttended = 0;
+		timesPaid = 0;
+		pAttended = rand.nextInt(10); // debug
+		timesPaid = rand.nextInt(10); // debug
+	}
+	
+    public void setNumber(String num){
+		phoneNumber = num;
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public coachViewer() {
-		setTitle("Coach Viewer");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 659, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JButton btnNewButton_4 = new JButton("Add Member");
-		btnNewButton_4.setBounds(171, 97, 147, 25);
-		contentPane.add(btnNewButton_4);
-		
-		JButton btnNewButton_5 = new JButton("Delete Member");
-		btnNewButton_5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String password = "abc";
-				JPanel panel = new JPanel();
-				JLabel label = new JLabel("Enter Member Name:");
-				JTextField memeberName = new JTextField(10);
-				JPasswordField passwordBox = new JPasswordField(10);
-				panel.add(label);
-				panel.add(memeberName);
-				String[] options = new String[]{"OK", "Cancel"};
-				int select = JOptionPane.showOptionDialog(null, panel, "Delete Member",
-				                         JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE,
-				                         null, options, options[1]);
-				if(select == 0) 
-				{
-				    String memName = memeberName.getText(); //gets memberName
-					 
-				
-					 try{
-					 
-				     
-				     
-				     
-					
-				     Scanner scanner = new Scanner(new File("logfile.txt"));
-				     
-				     List<String> lines = new ArrayList<String>();
-					 while(scanner.hasNextLine()){
-						 
-						 lines.add(scanner.nextLine());
-						 
-					 }
-					 
-					 for(int i = 0; i < lines.size(); i++) {   
-						    if(lines.get(i).equals(memName.trim())){
-						    	for(int it = 0;it < 6; it++){
-						    		lines.remove(i);
-						    	}
-						    }
-						} 
-					 
-					 
-                     Iterator<String> iterator = lines.iterator();
-                     PrintWriter writer = new PrintWriter("logfile.txt");
-                     while(iterator.hasNext()){
-						 writer.write(iterator.next() + "\n");
-					 }
-					 
-					
-					 writer.close();
-					 
-					 
-					 
-					 }catch(FileNotFoundException ex){
-						 System.out.println("File not found");  //change
-					 }catch(IOException ex){
-						 System.out.println(ex);
-					 }
-				}
-			
-			
-			}
-		});
-		btnNewButton_5.setBounds(334, 97, 147, 25);
-		contentPane.add(btnNewButton_5);
-		
-		JButton btnNewButton = new JButton("Notify Member");
-		btnNewButton.setBounds(498, 97, 147, 25);
-		contentPane.add(btnNewButton);
+	public void setPaymentStatus(boolean status){
+		paid = status;
 	}
+
+	public void setAddress(String address){
+        Address = address;
+	}
+
+	public void setMessage(String message){
+	    Message = message;
+	}
+
+	public void addActivity(String act)
+	{
+		activity= act;
+	}
+	
+	public String printActivity()
+	{
+		if(activity != null) return activity;
+		else return "Not enrolled";
+	}
+	
+	
+	public int getPractices()
+	{
+		return pAttended;
+	}
+	
+	public String toString()
+	{
+		String out = "";
+		out = Fname+" "+Lname+" "+" Practices: "+ pAttended +" Times Paid: "+ timesPaid +" Activity: " + printActivity() ;
+		return out; 
+	}
+	
+	public int getTimesPaid()
+	{
+		return timesPaid;
+	}
+	
+	@Override
+	public int compareTo(Member otherMember) {
+		if(otherMember.getPractices() > this.getPractices()) return -1;
+		else if(otherMember.getPractices() > this.getPractices()) return 1;
+		else return 0;
+	}
+	
+	public String getName()
+	{
+		return Fname + " " + Lname;
+	}
+	public String getFName(){
+		return Fname;
+	}
+
+	public String getLName(){
+		return Lname;
+	}
+
+	public String getNumber(){
+		return phoneNumber;
+	}
+
+    public String getPaymentStatus(){
+		
+		if(paid == true)
+		  return "paid";
+		else
+		  return "unpaid";
+	}
+
+	public String getAddress(){
+		return Address;
+	}
+
+    public String getMessage(){
+		return Message;
+	}
+
+	public void addMember(){     //add log of member to textfile containing member logs
+		
+		try{
+		FileWriter writer = new FileWriter ("logfile.txt" , true);  
+		writer.write(getName()+  "\n"); 
+		writer.write(getNumber()+ "\n");
+		writer.write(getPaymentStatus()+ "\n");
+		writer.write(getAddress()+ "\n");
+		writer.write(getMessage()+ "\n");
+		writer.write("" + "\n");
+		writer.flush();
+		writer.close();
+		}catch(FileNotFoundException e){
+			System.out.println("File not found."); //change
+		}catch(IOException e){
+			System.out.println(e);
+		}
+	}
+	
+	
 }
