f47c464a012a4b2ab22cdcb64c045a29dcfcf961
Kaushal Desai <kaushal@kaushals-mbp.wifi.int.ryerson.ca>
2018-04-05 11:34:49 -0400

    added treasurer methods

--- Treasurer.java
+++ Treasurer.java
@@ -1,4 +1,45 @@
-
 public class Treasurer {
 
+
+	private MembersList m;
+	private Coach c;
+
+	public Treasurer(MembersList m) {
+		this.m = m;
+
+
+
+	}
+
+
+
+	public void profit() {
+
+		double revenue = 0;
+		double expenses = 0;
+		double unPaid = 0;
+		double rent = 1000;
+		double coachesWage = getHoursWorked();
+
+
+
+		for(Members m : m.Members)
+		{
+			revenue += m.getPaid();
+			unPaid += m.getUnpaid();
+		}
+
+		expenses = unPaid + rent + coachesWage;
+
+		System.out.println("Revenue: $" + revenue )
+
+	}
+
+
+
+	
+
 }
+
+
+
