7c1c20337534483d1b4b02bc07463628cfef560e
= <=>
2018-04-06 00:02:30 -0400

    notification change

--- notificationViewer.java
+++ notificationViewer.java
@@ -12,10 +12,16 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import java.awt.*;
+import javax.swing.*;
+
+import java.io.*;
+
 public class notificationViewer extends JFrame {
 
 	private JPanel contentPane;
 	private Member mainMember;
+	private JTextField txtHh;
 	/**
 	 * Launch the application.
 	 */
@@ -39,13 +45,13 @@ public class notificationViewer extends JFrame {
 		mainMember=mem;
 		setTitle("Notification");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 148);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Notifications");
+		/* JLabel lblNewLabel = new JLabel("Notifications");
 		lblNewLabel.setBackground(Color.GRAY);
 		lblNewLabel.setBounds(22, 0, 122, 36);
 		contentPane.add(lblNewLabel);
@@ -53,10 +59,12 @@ public class notificationViewer extends JFrame {
 		JLabel label = new JLabel("     Messages");
 		label.setBounds(82, 48, 122, 36);
 		contentPane.add(label);
+
+		*/
 		
-		JLabel label_1 = new JLabel(mainMember.getMessage());
-		label_1.setBounds(92, 98, 239, 36);
-		contentPane.add(label_1);
+		//JLabel label_1 = new JLabel(mainMember.getMessage());
+		//label_1.setBounds(92, 98, 239, 36);
+		//contentPane.add(label_1);
 		
 		JButton btnNewButton = new JButton("<--");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -68,5 +76,16 @@ public class notificationViewer extends JFrame {
 		});
 		btnNewButton.setBounds(331, 12, 86, 25);
 		contentPane.add(btnNewButton);
+
+		txtHh = new JTextField();
+		txtHh.setFont(new Font("Dialog", Font.BOLD, 12));
+		txtHh.setHorizontalAlignment(SwingConstants.CENTER);
+		txtHh.setForeground(Color.BLACK);
+		txtHh.setText(mainMember.getMessage());
+		txtHh.setBackground(Color.LIGHT_GRAY);
+		txtHh.setEditable(false);
+		txtHh.setBounds(22, 49, 414, 58);
+		contentPane.add(txtHh);
+		txtHh.setColumns(10);
 	}
 }
