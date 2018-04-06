fa7804c26e0ca1e53624f4d3b0bee051475ca010
= <=>
2018-04-06 00:08:35 -0400

    form fix

--- memberViewer.java
+++ memberViewer.java
@@ -45,7 +45,7 @@ public class memberViewer extends JFrame {// Ahhhhhopppopoododoodododod
 
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 576, 300);
+		setBounds(100, 100, 576, 209);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
